67ad62d7315447b587736c9c2e8814739dd5e580
Dominique Fischer <122815547+DomUndAlf@users.noreply.github.com>
2026-05-07 14:16:38 +0200

    Update classes.kt

--- classes.kt
+++ classes.kt
@@ -1,5 +1,6 @@
 open class SmartDevice(val name: String, val category: String ) {
 	var statuscode = "unknown"
+    open val deviceType = "unknown"
     
     constructor (name: String, category: String, state: Int) : this(name, category)
     {
@@ -10,16 +11,17 @@ open class SmartDevice(val name: String, val category: String ) {
     }
     }
         
-    fun turnOn() {
-        println("Smart device is turned on.")
+    open fun turnOn() {
+        statuscode = "on"
     }
-    fun turnOff() {
-        println("Smart device is turned off.")
+    open fun turnOff() {
+        statuscode = "off"
     }
 }
 
-class SmartPhone (deviceName: String, deviceCategory: String) : 
+class SmartTv (deviceName: String, deviceCategory: String) : 
 SmartDevice(name = deviceName, category = deviceCategory){
+ override val deviceType = "Smart TV"   
     
  var channelNumber = 1
     set(value) {
@@ -27,27 +29,38 @@ SmartDevice(name = deviceName, category = deviceCategory){
             field = value
         }
     } 
-    
 var speakerVolume = 2
     set(value) {
         if (value in 0..100) {
             field = value
         }
     }
-
     fun increaseVolume() {
         speakerVolume++
         println("volume increased to $speakerVolume")
     }
-    
     fun nextChannel() {
         channelNumber++
         println("Channel number increased to $channelNumber.")
     }
+
+ override fun turnOn() {   
+    super.turnOn() 
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                "set to $channelNumber."
+        )
+    }
+ 
+ override fun turnOff() {
+    super.turnOff()
+        println("$name turned off")
+    }
 }
 
-class SmartLightDevice(deviceName: String, deviceCategory: String) :
+class SmartLight(deviceName: String, deviceCategory: String) :
 	SmartDevice(name = deviceName, category = deviceCategory) {
+        override val deviceType = "Smart Light"
         
         var brightnessLevel = 0
         set(value) {
@@ -60,21 +73,60 @@ class SmartLightDevice(deviceName: String, deviceCategory: String) :
         brightnessLevel++
         println("Brightness increased to $brightnessLevel.")
     }
+    override fun turnOn() {
+      	super.turnOn()   
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
     }
+    override fun turnOff() {
+        super.turnOff() 
+        brightnessLevel = 0
+        println("Smartlight turned off")
+    }
+}
 
-class SmartHome (val smartTvDevice: SmartTvDevice)  {
+class SmartHome (
+    val smartLight: SmartLight,
+    val smartTv: SmartTv
+)  {
+    fun turnOnTv() {
+        smartLight.turnOn()
+    }
+    fun turnOffTv() {
+    smartLight.turnOff()
+	}
+    fun increaseTvVolume() {
+        smartTv.increaseVolume()
+    }
+    fun changeTvChannelToNext() {
+        smartTv.nextChannel()
+    }
+    fun turnOnLight() {
+        smartLight.turnOn()
+    }
+    fun turnOffLight() {
+        smartLight.turnOff()
+    }
+    fun increaseLightBrightness() {
+        smartLight.increaseBrightness()
+    }
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
+    }
 }
 
 fun main() {
-    val tv = SmartDevice(name = "Fred", category = "human", state = 0)
- 
-    tv.turnOn()
-    tv.turnOff()
-    println ("devide name is ${tv.name} of category ${tv.category} and has status ${tv.statuscode}")
-
-    val phone = SmartPhone(deviceName = "Martina", deviceCategory = "human")
-    phone.increaseVolume()
-    phone.nextChannel()  
-    println(phone.name)
+   
+    val tv = SmartTv(deviceName = "Martina", deviceCategory = "human")
+    tv.increaseVolume()
+    tv.nextChannel()  
+    println(tv.name)
+    
+    var smartDevice: SmartDevice = SmartTv("Android TV", "Entertainment")
+    smartDevice.turnOn()
+    
+    smartDevice = SmartLight("Google Light", "Utility")
+    smartDevice.turnOn()
+   
 }
-
